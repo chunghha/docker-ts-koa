cb5f90bf5439bc158235e3a8ed63addd68482f7a
Chris Ha <chung.h.ha@gmail.com>
2017-04-30 11:07:08 -0500

    feat(route): koa-route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import * as Koa from 'koa'
 import * as logger from 'koa-logger'
+import * as KR from 'koa-route'
 
 const app = new Koa()
+const res = {
+  api: (ctx) => {
+    ctx.body = 'Hello, api'
+  },
+
+  root: (ctx) => {
+    ctx.body = 'Hello, World'
+  },
+}
 
 app.use(logger())
-app.use((ctx) => {
-  ctx.body = 'Hello, World'
-})
+app.use(KR.get('/', res.root))
+app.use(KR.get('/api', res.api))
 
 app.listen(8080)
